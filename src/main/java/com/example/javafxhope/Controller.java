b15e349c9e2dd91719ffc46c5d3868ab36ad3e0e
ulianaboikova <ulyanaboykova49@gmail.com>
2024-11-07 01:49:12 -0800

    Initial commit

---
+++ src/main/java/com/example/javafxhope/Controller.java
@@ -0,0 +1,21 @@
+package com.example.javafxhope;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
+
